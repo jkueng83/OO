23b8c3094bcfc48324254e0afa1d345be8cdb1db
jkueng83 <johannes.kueng@gmx.at>
2020-04-03 20:00:03 +0200

    first commit

---
+++ src/at/cc/jk/OO/Mensch/Main.java
@@ -0,0 +1,20 @@
+package at.cc.jk.OO.Mensch;
+/*
+Aufgabe Mensch
+
+Versucht folgende Vererbung abzubilden:
+Torwart || Sportler || Fußballer || Mensch || Stürmer || Büroangestellter
+
+Es gibt noch zwei weitere Klassen, nämlich einen Verein und eine Firma, welche auf entsprechender Ebene eine Zuordnung
+finden sollten.
+Findet Englische Begriffe dafür
+Überlegt euch in welcher Reihenfolge die Vererbung hier stattfinden kann
+Findet je Klasse zwei sinnvolle Eigenschaften und Methoden
+Achtet auf private/public
+
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println( "Aufgabe Mensch");
+    }
+}
