4cb0342315b33c545c90b5f53e4f4ff0ebf4f811
jkueng83 <johannes.kueng@gmx.at>
2020-04-18 08:52:17 +0200

    OO Aufgabe Verein UML mit draw.io ergänzt

--- src/at/cc/jk/OO/Verein/ClubCashier.java
+++ src/at/cc/jk/OO/Verein/ClubCashier.java
@@ -4,6 +4,7 @@ public class ClubCashier extends ClubMember {
 
     public ClubCashier(String firstName, String familyName, String birthday, float weight, float height) {
         super(firstName, familyName, birthday, weight, height);
+
         this.membershipFee = 100.00;
     }
 
